2d71d17f3c1d0d2eb56cb3affc8e4822a2124eaa
@zhejian123 <wangjunneil@gmail.com>
2023-10-19 03:17:34 -0500

    new script

---
+++ 签到-支付宝.js
@@ -0,0 +1,57 @@
+"auto";
+
+function unlock() {
+  if (!device.isScreenOn()) {
+    device.wakeUp();
+    sleep(500);
+    swipe(540, 2000, 540, 1200, 500);
+    sleep(500);
+    var password = "134416";
+    for (var i = 0; i < password.length; i++) {
+      var p = text(password[i].toString()).findOne().bounds();
+      click(p.centerX(), p.centerY());
+      sleep(200);
+    }
+
+    sleep(3000);
+  }
+}
+
+unlock();
+
+// ------------------------
+var isStarted = app.launchPackage("com.eg.android.AlipayGphone");
+sleep(3000);
+
+if (isStarted) {
+  click("Enter Alipay");
+  sleep(3000);
+
+  click("Me");
+  sleep(3000);
+
+  click(756, 539);
+  sleep(3000);
+
+  click("Get All");
+  sleep(3000);
+
+  className("android.widget.TextView").text("Daily Sign-in").waitFor();
+  className("android.widget.TextView").text("Daily Sign-in").findOne().click();
+
+  sleep(3000);
+
+  back();
+  sleep(500);
+  back();
+  sleep(1000);
+
+  click(0, 2238);
+  sleep(1000);
+
+  back();
+  sleep(500);
+  home();
+  sleep(100);
+  lockScreen();
+}
