5936374083f7e3c7c73db09a23e7b82f2e233632
哲剑 <zhejian.wj@alipay.com>
2023-10-20 13:28:02 +0800

    fix

--- test.js
+++ test.js
@@ -1,12 +1,1 @@
 // app.launchPackage("com.taobao.etao");
-
-function channel(channelName) {
-  //   var isClicked = textStartsWith(channelName).findOne().click();
-  var isClicked = className("android.view.View")
-    .text("浏览天猫超市频道 (0/1)")
-    .findOne()
-    .click();
-  console.log(isClicked);
-}
-
-channel("浏览天猫超市频道");
